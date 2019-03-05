be740128bd3a7f5679640d558501240cda818c00
Onion <s03351037@thu.edu.tw>
2019-03-05 16:47:08 +0800

    Create sidebar-Item.vue

---
+++ Sidebar/sidebar-Item.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-link :to="subMenu.url" active-class="active">
+    {{subMenu.name}}
+  </router-link>
+</template>
+<script>
+export default {
+  props: ['subMenu'],
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
