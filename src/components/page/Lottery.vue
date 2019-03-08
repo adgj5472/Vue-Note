7304944c190d3882c6953f68db311aa9d59f1e51
onion <adgj5472@gmail.com>
2019-03-09 00:32:04 +0800

    update lottery

--- src/components/page/Lottery.vue
+++ src/components/page/Lottery.vue
@@ -95,15 +95,29 @@ export default {
   methods: {
     Lottery: function () {
       this.winningList = []
-      document.getElementById('prize').innerHTML = ''
       var userdata = this.LotteryData.userdata
       var prize = this.LotteryData.prize
       var userdataLen = Object.keys(userdata).length
       var prizeLen = Object.keys(prize).length
+      var winArray = this.getRandomArray(0, userdataLen - 1, prizeLen)
       for (var i = 0; i < prizeLen; i++) {
-        var ran = Math.floor(Math.random() * userdataLen)
-        this.winningList.push({id: userdata[ran].id, name: userdata[ran].name, prizeId: prize[i].id, prizeName: prize[i].name})
+        var userIndex = winArray[i]
+        this.winningList.push({id: userdata[userIndex].id, name: userdata[userIndex].name, prizeId: prize[i].id, prizeName: prize[i].name})
       }
+    },
+    getRandomArray: function (minNum, maxNum, n) {
+      var rdmArray = [n] // 儲存產生的陣列
+      for (var i = 0; i < n; i++) {
+        var rdm = 0 // 暫存的亂數
+        do {
+          var exist = false // 此亂數是否已存在
+          rdm = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum // 取得亂數
+          // 檢查亂數是否存在於陣列中，若存在則繼續回圈
+          if (rdmArray.indexOf(rdm) !== -1) exist = true
+        } while (exist) // 產生沒出現過的亂數時離開迴圈
+        rdmArray[i] = rdm
+      }
+      return rdmArray
     }
   }
 }
